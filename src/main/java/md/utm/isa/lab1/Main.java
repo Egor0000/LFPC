e820529d3c983a410be497e445b0076f78798847
Egor_Babcinetchi <egor.babcinetchi@isa.utm.md>
2022-01-31 11:12:33 +0200

    add file fixes

--- src/main/java/md/utm/isa/lab1/Main.java
+++ src/main/java/md/utm/isa/lab1/Main.java
@@ -1,8 +1,8 @@
 package md.utm.isa.lab1;
 
 public class Main {
-    public static void main(String args[]) throws Exception{
-        GraphBuilder graphBuilder = new GraphBuilder("D:/EGOR/Projects/LFPC/lfpc/src/main/java/md/utm/isa/lab1/data.txt");
+    public static void main(String[] args) throws Exception{
+        GraphBuilder graphBuilder = new GraphBuilder("data.txt");
         graphBuilder.drawGraph();
     }
 
